9a0e9667b1f2bfd3329332ca3f421833690a1eba
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-08-07 01:08:11 +0630

    Add Proper Serialization to River Height Record

--- app/serializers/river_height_record_serializer.rb
+++ app/serializers/river_height_record_serializer.rb
@@ -1,3 +1,15 @@
 class RiverHeightRecordSerializer < ActiveModel::Serializer
-  attributes :id, :city_id, :river_id, :record_time, :description, :feet_above, :prediction
+  attributes :id, :record_time, :description, :feet_above, :prediction
+  
+  has_one :city
+  has_one :river
+  # def city
+  #   c = object.city
+  #   {mm_name: c.mm_name, en_name: c.en_name}.to_json
+  # end
+
+  # def river
+  #   r = object.river
+  #   {mm_name: r.mm_name, en_name: r.en_name}.to_json
+  # end
 end
